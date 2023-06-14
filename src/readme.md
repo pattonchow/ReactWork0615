7117d46be188187b4fedec498f8c7bc610bda6f8
Yao (Patton) Zhou <40862752+pattonchow@users.noreply.github.com>
2023-06-14 17:19:59 -0400

    Initial commit

---
+++ src/readme.md
@@ -0,0 +1,7 @@
+Implement a counter in redux.
+
+1. Counter can add by 1.
+2. Counter can minus by 1.
+3. Counter can add only by 1 when current count is odd.
+4. Counter can add after 1 seconds.
+   Please see the video in group chat to know what UI looks like.
---
+++ src/reducers.js
@@ -0,0 +1,24 @@
+import { INCREMENT, DECREMENT } from "./actions";
+
+// Initial State
+const initialState = {
+  count: 0
+};
+
+// Reducer
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        count: state.count + 1
+      };
+    case DECREMENT:
+      return {
+        count: state.count - 1
+      };
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
